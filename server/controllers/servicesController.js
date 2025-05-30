3fccc0a1d64755c5693f2ab62039e6451bd408d4
pritam <pritamstha26@gmail.com>
2025-05-30 12:23:50 +0545

    Feat : update delete get service by id added

--- server/controllers/servicesController.js
+++ server/controllers/servicesController.js
@@ -18,3 +18,50 @@ export async function getServices(req, res) {
     res.status(500).json({ error: error.message });
   }
 }
+
+// Get service by ID
+export async function getServiceById(req, res) {
+  try {
+    const id = req.params.id;
+    const service = await ServiceModel.findByPk(id);
+    if (!service) {
+      return res.status(404).json({ error: "Service not found" });
+    }
+    res.json(service);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+// Update service by ID
+export async function updateServiceById(req, res) {
+  try {
+    const id = req.params.id;
+    const [updated] = await ServiceModel.update(req.body, { where: { id } });
+
+    if (updated === 0) {
+      return res.status(404).json({ error: "Service not found" });
+    }
+
+    // Fetch updated service
+    const updatedService = await ServiceModel.findByPk(id);
+    res.json(updatedService);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export async function deleteServiceById(req, res) {
+  try {
+    const id = req.params.id;
+    const deleted = await ServiceModel.destroy({ where: { id } });
+
+    if (deleted === 0) {
+      return res.status(404).json({ error: "Service not found" });
+    }
+
+    res.json({ message: "Service deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
--- server/routes/serviceRoutes.js
+++ server/routes/serviceRoutes.js
@@ -1,7 +1,17 @@
 import express from "express";
-import { getServices, addService } from "../controllers/servicesController.js";
+import {
+  getServices,
+  addService,
+  getServiceById,
+  updateServiceById,
+  deleteServiceById,
+} from "../controllers/servicesController.js";
 const router = express.Router();
 // Route for user registration
 router.post("/", addService); // Route to add a new service
 router.get("/", getServices); // Route to get all services
+router.get("/:id", getServiceById); // Route to get a service by
+router.put("/:id", updateServiceById); // Route to update a service by ID
+router.delete("/:id", deleteServiceById); // Route to delete a service by ID
+
 export default router;
